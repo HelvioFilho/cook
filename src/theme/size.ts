1efd05b81bc8ca706cc7666b004e28fda38df2b5
Hélvio Filho <helviosvf@gmail.com>
2024-03-06 17:18:43 -0300

    chore: add custom sizes

---
+++ src/theme/size.ts
@@ -0,0 +1,11 @@
+export const fontSize = {
+  bodyXs: "12px",
+  bodySm: "14px",
+  bodyMd: "16px",
+
+  headingXs: "16px",
+  headingSm: "18px",
+  headingMd: "20px",
+  headingLg: "24px",
+  headingXl: "36px",
+};
