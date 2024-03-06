b7b2009b8da368fa3845080a290211357261e6ff
Hélvio Filho <helviosvf@gmail.com>
2024-03-06 17:23:08 -0300

    feat: add title and subtitle

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -2,8 +2,14 @@ import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View>
-      <Text className="text-3xl font-bold">Home</Text>
+    <View className="flex-1 p-6">
+      <Text className="text-headingXl font-bold leading-[44px] mt-10">
+        Escolha {"\n"}
+        <Text className="font-regular">os produtos</Text>
+      </Text>
+      <Text className="font-regular text-bodyMd text-gray-400 mt-3 mb-9">
+        Descubra receitas baseadas nos produtos que você escolheu.
+      </Text>
     </View>
   );
 }
