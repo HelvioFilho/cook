595e294879f1ca64c6652deab3e5272d018957b6
Hélvio Filho <helviosvf@gmail.com>
2024-03-06 17:21:51 -0300

    chore: add sizes to the tailwind configuration file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+import { colors } from "./src/theme/colors";
+import { fontFamily } from "./src/theme/fontFamily";
+import { fontSize } from "./src/theme/size";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+      fontFamily,
+      fontSize,
+    },
   },
   plugins: [],
 };
