7eea943827bb167dc7bbeb2e50631f906ebb2725
Hélvio Filho <helviosvf@gmail.com>
2024-03-07 09:08:34 -0300

    feat: shows the recipe ingredients

--- src/app/recipe/[id].tsx
+++ src/app/recipe/[id].tsx
@@ -1,23 +1,37 @@
 import { useEffect, useState } from "react";
-import { Image } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { Loading } from "@/components/Loading";
+import { Ingredients } from "@/components/ingredients";
 
 import { services } from "@/services";
 
 export default function Recipes() {
   const [isLoading, setIsLoading] = useState(true);
   const [recipe, setRecipe] = useState<RecipeResponse | null>(null);
+  const [ingredients, setIngredients] = useState<IngredientResponse[]>([]);
 
   const { id } = useLocalSearchParams<{ id: string }>();
+  const { back } = useRouter();
+
+  function handleBack() {
+    back();
+  }
 
   useEffect(() => {
     services.recipes
       .show(id)
       .then((response) => setRecipe(response))
       .finally(() => setIsLoading(false));
+  }, []);
+
+  useEffect(() => {
+    services.ingredients
+      .findByRecipeId(id)
+      .then((response) => setIngredients(response));
   }, []);
 
   if (isLoading) {
@@ -34,6 +48,17 @@ export default function Recipes() {
         className="w-full h-48 bg-gray-300"
         source={{ uri: recipe?.image }}
       />
+      <View className="bg-white -mt-6 rounded-t-3xl">
+        <View className="p-8">
+          <MaterialIcons name="arrow-back" size={32} onPress={handleBack} />
+          <Text className="font-bold text-headingMd mt-5">{recipe?.name}</Text>
+          <Text className="font-regular text-bodySm text-gray-400">
+            {recipe?.minutes} minutos de preparo
+          </Text>
+        </View>
+
+        <Ingredients ingredients={ingredients} />
+      </View>
     </SafeAreaView>
   );
 }
