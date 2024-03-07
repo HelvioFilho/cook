99a71f5be273506786bd776b57624147b7657d7b
Hélvio Filho <helviosvf@gmail.com>
2024-03-07 08:40:18 -0300

    feat: shows recipes based on ingredients or warns that there is no recipe

--- src/app/recipes/[ingredients].tsx
+++ src/app/recipes/[ingredients].tsx
@@ -1,23 +1,38 @@
-import { Loading } from "@/components/Loading";
-import { Ingredients } from "@/components/ingredients";
-import { services } from "@/services";
+import { useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useLocalSearchParams, useRouter } from "expo-router";
-import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+
+import { Loading } from "@/components/Loading";
+import { Recipe } from "@/components/Recipe";
+import { Ingredients } from "@/components/ingredients";
+
+import { services } from "@/services";
 
 export default function Recipes() {
   const [ingredients, setIngredients] = useState<IngredientResponse[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [recipes, setRecipes] = useState<RecipeResponse[]>([]);
 
-  const { back } = useRouter();
+  const { back, navigate } = useRouter();
   const params = useLocalSearchParams<{ ingredients: string }>();
   const ingredientsId = params.ingredients.split(",");
 
   function handleBack() {
     back();
   }
+
+  function handleNavigate(id: string) {
+    navigate("/recipe/" + id);
+  }
+
+  useEffect(() => {
+    services.recipes
+      .findByIngredientsIds(ingredientsId)
+      .then((response) => setRecipes(response))
+      .finally(() => setIsLoading(false));
+  }, []);
 
   useEffect(() => {
     services.ingredients
@@ -42,6 +57,24 @@ export default function Recipes() {
       </View>
 
       <Ingredients ingredients={ingredients} />
+
+      <FlatList
+        data={recipes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Recipe recipe={item} onPress={() => handleNavigate(item.id)} />
+        )}
+        className="p-8"
+        contentContainerStyle={{ gap: 16 }}
+        showsVerticalScrollIndicator={false}
+        columnWrapperStyle={{ gap: 16 }}
+        numColumns={2}
+        ListEmptyComponent={() => (
+          <Text className="text-gray-400 font-regular text-bodyMd">
+            Nenhuma receita encontrada. Escolha outros ingredientes.
+          </Text>
+        )}
+      />
     </SafeAreaView>
   );
 }
